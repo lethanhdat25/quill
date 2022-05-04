dd3882f84a5c0c130fb97857810eb5d137b740d0
lethanhdat25 <isaacbest.ig@gmail.com>
2022-05-04 21:08:43 +0700

    feat: up code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import {useState} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [body,setBody] = useState("");
+    console.log(body);
+    return (
+        <div className="App">
+            <ReactQuill
+                placeholder={"some thing like .... "}
+                value={body}
+                onChange={(e)=>setBody(e)}
+                modules={App.modules}
+                formats={App.formats}
+            />
+        </div>
+    );
+
+
+
 }
+
+App.modules = {
+    toolbar:[
+        [{header:"1"},{header:"2"},{header:[3,4,5,6]},{font:[]}],
+        [{size:[]}],
+        ["bold","italic","underline","strike","blockquote"],
+        [{list:"ordered"},{list:"bullet"}],
+        ["link","image","video"],
+        ["clean"],
+        ["code-block"],
+    ],
+};
+App.formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "link",
+    "image",
+    "video",
+    "code-block"
+]
 
 export default App;
